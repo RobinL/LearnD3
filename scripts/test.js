0096b1b14e8db95e12441530db7bfa9f55a7855e
Robin Linacre <robinlinacre@hotmail.com>
2013-01-27 16:49:45 +0000

    Fits to browser window, animated circles

--- scripts/test.js
+++ scripts/test.js
@@ -1,29 +1,35 @@
 
-window.onload = function() {
 
-w = window.innerWidth-5;
-h = window.innerHeight-5;
-
+w = window.innerWidth-25;
+h = window.innerHeight-25;
 padding = 40;
 
 svg = d3.select("body")
 	.append("svg")
-	.attr("width", w-20)
-	.attr("height", h-20)
+	.attr("width", w)
+	.attr("height", h);
 
 w = svg[0][0]["clientWidth"];
 h = svg[0][0]["clientHeight"];
 
 var dataset  = [];
-for (var i = 0; i < 100; i++) {
-	dataset.push(Math.random()*100);
+for (var i = 0; i < 150; i++) {
+	dataset.push(
+		[Math.random()*100, Math.random()*100, Math.random()*100]);
 }
 
 
-//domain goes in
+//should try to use the nice function
+//domainMin = d3.min(dataset);
+//domainMax = d3.max(dataset);
 
-domainMin = d3.min(dataset);
-domainMax = d3.max(dataset);
+
+
+/////////////////////////////////////////////
+//Axes
+//////////////////////////////////////////////
+
+
 
 domainMin = 0;
 domainMax = 100;
@@ -67,6 +73,93 @@ yAxisSvg.attr("transform","translate(" + padding + ",0)");
 
 
 
+/////////////////////////////////////////////
+//Plotpoints
+//////////////////////////////////////////////
+
+var circles = svg.selectAll("circle")
+				.data(dataset)
+				.enter()
+				.append("circle");
 
 
-};
+
+circles.attr("cx", function(d) {
+	return xScale(d[0]);
+	})
+	.attr("cy", function(d) {
+		return yScale(d[1]);
+	});
+
+
+
+domainMin = d3.min(dataset,function(d) {return (d [2]);});
+domainMax = d3.max(dataset,function(d) {return (d [2]);});
+
+var circleScale = d3.scale.linear()
+					.domain([domainMin, domainMax])
+					.range([2,20]);
+
+circles.attr("r",function(d) {
+		return circleScale(d[2]);
+	});
+
+var circleColourScale = d3.scale.linear()
+							.domain([domainMin, domainMax])
+							.range([0,255]);
+
+circles.attr("fill", function(d){
+
+	col = circleColourScale(d[2]);
+	return d3.rgb(255-col,col,col);
+
+});
+
+circles.attr("stroke", function(d){
+
+	col = circleColourScale(d[2]);
+	return d3.rgb(col,255-col,255);
+
+});
+
+
+/////////////////////////////////////////////
+//Updates and transitions 
+//////////////////////////////////////////////
+
+
+d3.select("svg")
+.on("click", function() {
+
+var dataset  = [];
+for (var i = 0; i < 150; i++) {
+	dataset.push(
+		[Math.random()*100, Math.random()*100, Math.random()*100]);
+}
+
+
+svg.selectAll("circle")
+	.data(dataset)
+	.transition()
+	.duration(2000)
+	.attr("cx", function(d) {
+	return xScale(d[0]);
+	})
+	.attr("cy", function(d) {
+		return yScale(d[1]);
+	})
+	.attr("fill", function(d){
+	col = circleColourScale(d[2]);
+	return d3.rgb(col,255-col,255);})
+	.attr("r",function(d) {
+		return circleScale(d[2]);
+	})
+
+
+
+
+
+
+
+
+});
